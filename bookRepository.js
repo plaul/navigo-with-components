57c283e8f603c5853f9ceb35f5f9ca8973293b3d
plaul <lmor@kea.dk>
2021-11-15 14:42:15 +0100

    Update bookRepository.js

--- bookRepository.js
+++ bookRepository.js
@@ -22,8 +22,7 @@ function bookRepository() {
 
   const findBook = async (id) => {
     if (books.length === 0) {
-      const book = await fetch(URL + "/" + id).then(res => handleHttpError(res, "Could not find a book with the ID: " + id))
-      return book;
+      return await fetch(URL + "/" + id).then(res => handleHttpError(res, "Could not find a book with the ID: " + id))
     } else {
       const book = books.find(book => book.id == id)
       if (!book) throw new Error("Could not find a book with the ID: " + id)
@@ -37,15 +36,15 @@ function bookRepository() {
     books = books.filter(book => book.id != id)
   }
 
-  const addBook = async (book) => {
-    const options = makeOptions("POST", book)
+  const addBook = async (bookToAdd) => {
+    const options = makeOptions("POST", bookToAdd)
     const addedBook = await fetch(URL, options).then(res => handleHttpError(res, `Error '${res.status} `))
     books.length === 0 ? await getBooks() : books.push(addedBook)
     return addedBook
   }
 
-  const editBook = async (book, id) => {
-    const options = makeOptions("PUT", book)
+  const editBook = async (bookToEdit, id) => {
+    const options = makeOptions("PUT", bookToEdit)
     const editedBook = await fetch(URL + "/" + id, options).then(res => handleHttpError(res, `Error '${res.status} `))
     books = books.map(book => book.id == id ? editedBook : book)
   }
